0aaaae4f66793abfc11773b00980219d9e822828
akestler <andrew.kestler@generalassemb.ly>
2022-10-21 12:03:26 -0400

    commented for code review, see  pull

--- models/character.js
+++ models/character.js
@@ -1,17 +1,17 @@
 const mongoose = require('./connection')
 
 // import user model for populate
-const User = require('./user')
+const User = require('./user') // remove unused imports
 
 // destructure the schema and model constructors from mongoose
 const { Schema, model } = mongoose
 
-const characterSchema = new Schema(
+const characterSchema = new Schema( // capitalize our schemas ! they are JS classes
     {
         name: { type: String, required: true},
         desc: { type: String},
         owner: { type: Schema.Types.ObjectID, ref: 'User', },
-        anime: { type: Schema.Types.ObjectID, ref: 'Anime', }
+        anime: { type: Schema.Types.ObjectID, ref: 'Anime', } // a little confused here, if your intention is the origin/ native anime for the character then i'd suggest using a different name for this variable
         
     },
     { timestamps: true}
